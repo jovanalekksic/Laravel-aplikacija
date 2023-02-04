0d8faff8129a1ec5e77064d2933f86dc47f12abf
Jovana Aleksic <jaleksic23@gmail.com>
2023-02-04 15:08:16 +0100

    postovi jednog korisnika

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\BookController2;
+use App\Http\Controllers\UserBookController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::get('/books', [BookController::class, 'index']);
 
 Route::resource('books', BookController2::class);
+
+Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('users/{id}/posts', [UserBookController::class, 'index'])->name('users.books.index');
